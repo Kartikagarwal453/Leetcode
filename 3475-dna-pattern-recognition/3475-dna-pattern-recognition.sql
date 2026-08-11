87fcbc82bcbbc7c8c238b9e679810e46b8d66500
Kartik Agarwal <agarwalkartik453@gmail.com>
2026-08-11 21:26:23 +0530

    Time: 218 ms (28.6%), Space: 0B (100%) - LeetHub

---
+++ 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
@@ -0,0 +1,15 @@
+SELECT
+    sample_id,
+    dna_sequence,
+    species,
+
+    (dna_sequence ~ '^ATG')::int AS has_start,
+
+    (dna_sequence ~ '(TAA|TAG|TGA)$')::int AS has_stop,
+
+    (dna_sequence LIKE '%ATAT%')::int AS has_atat,
+
+    (dna_sequence ~ 'G{3,}')::int AS has_ggg
+
+FROM Samples
+ORDER BY sample_id ASC;
