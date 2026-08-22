5b4a2230253258c122bfb2e00dd51f5319fe0f8b
Kartik Agarwal <agarwalkartik453@gmail.com>
2026-08-22 22:07:36 +0530

    Time: 287 ms (13.57%), Space: 0B (100%) - LeetHub

--- 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
+++ 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
@@ -1,22 +1,11 @@
-SELECT
-    lb.book_id,
-    lb.title,
-    lb.author,
-    lb.genre,
-    lb.publication_year,
-    COUNT(br.record_id) AS current_borrowers
-FROM library_books lb
-JOIN borrowing_records br
-    ON lb.book_id = br.book_id
-    AND br.return_date IS NULL
-GROUP BY
-    lb.book_id,
-    lb.title,
-    lb.author,
-    lb.genre,
-    lb.publication_year,
-    lb.total_copies
-HAVING COUNT(br.record_id) = lb.total_copies
-ORDER BY
-    current_borrowers DESC,
-    lb.title ASC;
+WITH CurrentBorrows AS(
+    SELECT book_id,COUNT(*) AS current_borrowers
+    FROM borrowing_records
+    WHERE return_date IS NULL
+    GROUP BY book_id
+)
+SELECT l.book_id,l.title,l.author,l.genre,l.publication_year,c.current_borrowers
+FROM library_books l
+JOIN CurrentBorrows c ON l.book_id=c.book_id
+WHERE c.current_borrowers=l.total_copies
+ORDER BY c.current_borrowers DESC,l.title ASC;
